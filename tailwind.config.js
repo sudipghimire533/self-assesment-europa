3f6a00625dd4b0884bd5ba3f407f795d4c4925f1
sudipghimire533 <sudipghimire533@users.noreply.github.com>
2023-11-03 00:24:40 +0000

    deploy: c469a9acdaa7003d7deb1a090381a8053c1a14e6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#F1F4ED'
-      }
-    }
-  },
-  plugins: [],
-}
-
+module.exports={content:["./src/**/*.{html,js}"],theme:{extend:{colors:{primary:'#F1F4ED'}}},plugins:[]}
